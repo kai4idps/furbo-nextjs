cd906fcbdeb1d5fdee618723be87954a827224e6
chekai7 <rockkcor75@gmail.com>
2021-11-30 14:57:47 +0800

    lazy loading react-gtm-module

--- pages/_app.js
+++ pages/_app.js
@@ -8,20 +8,28 @@ import theme from 'styles/theme';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import 'styles/global.css';
 import styles from 'styles/mainStyle';
-// import { GTM_DELAY_TIME } from 'config/common';
-// import { GTM_ID } from 'config/gtm';
-// import TagManager from 'react-gtm-module';
+import { GTM_DELAY_TIME } from 'config/common';
+import { GTM_ID } from 'config/gtm';
+import TagManager from 'react-gtm-module';
 
 const useStyles = makeStyles(styles);
 
 const MyApp = ({ Component, pageProps }) => {
   const classes = useStyles();
-  // const tagManagerArgs = {
-  //   id: GTM_ID,
-  // };
-  // useEffect(() => {
-  //   TagManager.initialize(tagManagerArgs);
-  // }, []);
+  const tagManagerArgs = {
+    gtmId: GTM_ID,
+  };
+  useEffect(() => {
+    const initGtm = () => {
+      TagManager.initialize(tagManagerArgs);
+    };
+    document.addEventListener('DOMContentLoaded', () => {
+      document.addEventListener('scroll', initGtmOnEvent);
+      document.addEventListener('mousemove', initGtmOnEvent);
+      document.addEventListener('touchstart', initGtmOnEvent);
+      setTimeout(initGtm, GTM_DELAY_TIME);
+    });
+  }, []);
   // useEffect(() => {
   //   const initGtmOnEvent = (event) => {
   //     initGtm();
@@ -46,12 +54,12 @@ const MyApp = ({ Component, pageProps }) => {
   //     script.src = 'https://www.googletagmanager.com/gtm.js?id=GTM-N37W29C';
   //     document.head.appendChild(script);
   //   };
-  //   document.addEventListener('DOMContentLoaded', () => {
-  //     document.addEventListener('scroll', initGtmOnEvent);
-  //     document.addEventListener('mousemove', initGtmOnEvent);
-  //     document.addEventListener('touchstart', initGtmOnEvent);
-  //     setTimeout(initGtm, GTM_DELAY_TIME);
-  //   });
+  // document.addEventListener('DOMContentLoaded', () => {
+  //   document.addEventListener('scroll', initGtmOnEvent);
+  //   document.addEventListener('mousemove', initGtmOnEvent);
+  //   document.addEventListener('touchstart', initGtmOnEvent);
+  //   setTimeout(initGtm, GTM_DELAY_TIME);
+  // });
   // }, []);
 
   return (
