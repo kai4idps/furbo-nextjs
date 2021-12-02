376cfbdb859e98dffe3ad51c23c8fade0851744d
chekai7 <rockkcor75@gmail.com>
2021-12-02 17:06:04 +0800

    Fix document

--- pages/_document.js
+++ pages/_document.js
@@ -1,7 +1,6 @@
 import { Children } from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheets } from '@material-ui/core/styles';
-import { GTM_ID } from 'config/gtm';
 
 class MyDocument extends Document {
   render() {
