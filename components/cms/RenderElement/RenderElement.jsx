c5392afb197ccf439ad082cd1a3878179311c573
chekai7 <rockkcor75@gmail.com>
2021-12-02 18:31:33 +0800

    RM node module

--- components/cms/RenderElement/RenderElement.jsx
+++ components/cms/RenderElement/RenderElement.jsx
@@ -42,13 +42,14 @@ const RenderElement = ({ type, data }) => {
       );
     case 'product_button':
       return (
-        <ProductButton
-          productInfo={data[`product_info_${region}`]}
-          productName={data.product_name}
-          productType={data.product_type}
-          productText={data.product_text}
-          buttonText={data.button_text}
-        />
+        <></>
+        // <ProductButton
+        //   productInfo={data[`product_info_${region}`]}
+        //   productName={data.product_name}
+        //   productType={data.product_type}
+        //   productText={data.product_text}
+        //   buttonText={data.button_text}
+        // />
       );
     case 'shop_button':
       return (
