787650e562e34f670ce2c800bcf44fbde0577ca9
chekai7 <rockkcor75@gmail.com>
2021-12-06 15:21:00 +0800

    Fix card page and product key visual

--- components/cms/ProductKeyVisual/ProductKeyVisual.jsx
+++ components/cms/ProductKeyVisual/ProductKeyVisual.jsx
@@ -1,4 +1,5 @@
-import { Children } from 'react';
+import { Children, useMemo } from 'react';
+import { useRouter } from 'next/router';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import { RichText } from 'prismic-reactjs';
@@ -7,12 +8,40 @@ import Hidden from '@material-ui/core/Hidden';
 import VisibleSensorAnimation from 'components/VisibleSensorAnimation';
 import RenderElement from 'components/cms/RenderElement';
 import ProductSlide from './ProductSlide';
+import ShopButton from 'components/button/ShopButton';
+import { isEmpty } from 'src/helpers';
 import styles from './productKeyVisualStyle';
 
 const useStyles = makeStyles(styles);
 
 const ProductKeyVisual = ({ productPage }) => {
   const classes = useStyles();
+  const router = useRouter();
+  const { region, productId } = router.query;
+
+  const productInfo = useMemo(() => {
+    return isEmpty(productId)
+      ? productPage.product_list[0][`product_info_${region}`]
+      : productPage.product_list.find(
+          (item) => +item[`product_info_${region}`].product_id === +productId,
+        )?.[`product_info_${region}`] ||
+          productPage.product_list[0][`product_info_${region}`];
+  }, [productId, region, productPage.product_list]);
+
+  const slides = isEmpty(productId)
+    ? productPage.product_slides
+    : [
+        {
+          slide: {
+            height: 500,
+            width: 500,
+            kind: 'image',
+            url: productInfo.image.src,
+          },
+        },
+        ...productPage.product_slides.slice(1),
+      ];
+
   return (
     <VisibleSensorAnimation animation="grow">
       <div className={classes.root}>
@@ -26,7 +55,7 @@ const ProductKeyVisual = ({ productPage }) => {
             sm={12}
             xs={12}
           >
-            <ProductSlide slides={productPage.product_slides} />
+            <ProductSlide slides={slides} />
           </Grid>
           <Grid
             className={classes.rightGrid}
@@ -39,30 +68,44 @@ const ProductKeyVisual = ({ productPage }) => {
           >
             <Hidden smDown>
               <div className={classes.title}>
-                {RichText.render(productPage.title)}
+                {isEmpty(productId) ? (
+                  RichText.render(productPage.title)
+                ) : (
+                  <h1>{productInfo.title}</h1>
+                )}
               </div>
               <div className={classes.subtitle}>
-                {RichText.render(productPage.subtitle)}
+                {isEmpty(productId) ? (
+                  RichText.render(productPage.subtitle)
+                ) : (
+                  <p>{productInfo.description}</p>
+                )}
               </div>
-              <div
-                className={classes.colorText}
-                style={{
-                  color: `${productPage.text_color}`,
-                }}
-              >
-                {RichText.render(productPage.color_text)}
-              </div>
+              {isEmpty(productId) && (
+                <div
+                  className={classes.colorText}
+                  style={{
+                    color: `${productPage.text_color}`,
+                  }}
+                >
+                  {RichText.render(productPage.color_text)}
+                </div>
+              )}
             </Hidden>
-            {Children.toArray(
-              productPage.product_button.map((item, index) => (
-                <>
-                  {index !== 0 && <br />}
-                  <RenderElement
-                    type={item.button.type}
-                    data={item.button.data}
-                  />
-                </>
-              )),
+            {isEmpty(productId) &&
+              Children.toArray(
+                productPage.product_button.map((item, index) => (
+                  <>
+                    {index !== 0 && <br />}
+                    <RenderElement
+                      type={item.button.type}
+                      data={item.button.data}
+                    />
+                  </>
+                )),
+              )}
+            {!isEmpty(productId) && (
+              <ShopButton className={classes.shopButton} text={'Shop Now'} />
             )}
           </Grid>
         </Grid>
