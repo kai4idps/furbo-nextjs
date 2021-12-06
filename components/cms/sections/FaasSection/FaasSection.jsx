aee3cb61dc24b489501a9976f94092a034e73479
chekai7 <rockkcor75@gmail.com>
2021-12-06 15:48:05 +0800

    Fix FaasSection

--- components/cms/sections/FaasSection/FaasSection.jsx
+++ components/cms/sections/FaasSection/FaasSection.jsx
@@ -1,8 +1,14 @@
 import { Children } from 'react';
 import dynamic from 'next/dynamic';
-const DynamicRenderSlice = dynamic(() => import('components/cms/RenderSlice'), {
-  loading: () => <></>,
-});
+// const DynamicRenderSlice = dynamic(() => import('components/cms/RenderSlice'), {
+//   loading: () => <></>,
+// });
+const DynamicRenderSlice = dynamic(
+  () => import('components/cms/RenderSlice').then((mod) => mod.RenderSlice),
+  {
+    loading: () => <></>,
+  },
+);
 
 const FaasSection = ({ faasPage }) => {
   return (
